4dc7916aafd505e47f4c9b6b65a70ae49172a14c
om salunke <omsaunke343@gmail.com>
2024-09-29 23:39:24 +0530

    simple number pattern uploaded

--- number/pattern_1.cpp
+++ number/pattern_1.cpp
@@ -1,12 +1,11 @@
 /*
-*
-**
-***
-****
-*****
+1
+12
+123
+1234
+12345
 
 */
-
 #include <iostream>
 using namespace std;
 int main()
@@ -14,12 +13,15 @@ int main()
     int n;
     cout << "Enter no of rows:";
     cin >> n;
+    int count = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i + 1; j++)
         {
-            cout << "*";
+            count++;
+            cout << count;
         }
+        count = 0;
         cout << endl;
     }
 
